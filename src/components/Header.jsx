0932b254bf69033d29f46c03799c4ca87e92deed
entegral <e2robby@gmail.com>
2019-02-11 13:34:06 -0800

    add nav-bar layout for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
 function Header(){
+  
+  const componentStyle = {
+    display: "inline",
+  }
+
+  const linkStyle = {
+    textDecoration: "none",
+    color: "black"
+  }
+  
+  
   return (
     <div>
-      <h1>Header Component Works</h1>
+      <ul>
+        <li style={componentStyle}><a style={linkStyle} href="#">Home | </a></li>
+        <li style={componentStyle}><a style={linkStyle} href="#">Link | </a></li>
+        <li style={componentStyle}><a style={linkStyle} href="#">Contact | </a></li>
+        <li style={componentStyle}><a style={linkStyle} href="#">Login </a></li>
+      </ul>
       <hr/>
     </div>
   );
