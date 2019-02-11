44f786e5bf93776a5cffb38022b028d10df186c0
entegral <e2robby@gmail.com>
2019-02-11 10:53:24 -0800

    add App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,8 @@ import React from "react";
 function App() {
   return (
     <div>
-      <h1>Help Queue</h1>
-      <h3>3a</h3>
-      <h3>Thato and Haley</h3>
-      <p><em>Firebase entries not saving!</em></p>
-      <hr />
+      <h1>Generic Template Works!</h1>
+      <hr/>
     </div>
   );
 }
