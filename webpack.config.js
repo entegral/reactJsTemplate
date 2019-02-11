c23898e3acc2b7b870d2ce0021c76050ada2f881
entegral <e2robby@gmail.com>
2019-02-11 10:37:48 -0800

    configure babel with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { resolve } = require('path');
 module.exports = {
 
   entry: [
-    resolve(__dirname, src) + /index.jsx
+    resolve(__dirname, "src") + "/index.jsx"
   ],
 
   output: {
@@ -14,7 +14,21 @@ module.exports = {
 
   resolve: {
     extensions: ['.js', '.jsx']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            "es2015",
+            "react"
+          ]
+        }
+      },
+    ],
   }
-
 };
-
