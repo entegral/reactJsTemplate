aceeda456c095ceeb3ed531893822355a6593a08
entegral <e2robby@gmail.com>
2019-02-11 13:11:18 -0800

    add nested generic header component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from './Header'
 
 function App() {
-  return (
+  return (  
     <div>
+      <Header/>
       <h1>Generic Template Works!</h1>
       <hr/>
     </div>
