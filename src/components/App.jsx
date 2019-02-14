45ef032b544506eed8b4752d6983a84506434e0b
entegral <e2robby@gmail.com>
2019-02-14 10:13:17 -0800

    add react router with example homepage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from './Header';
+import Home from './Home';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (  
     <div>
       <Header/>
-      <h1>Generic Template Works!</h1>
+      <Switch>
+        <Route exact path='/' component={Home} />
+
+        <h1>Generic Template Works!</h1>
+      </Switch>
       <hr/>
     </div>
   );
